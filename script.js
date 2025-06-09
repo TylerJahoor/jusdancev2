7bb27b6b07f23c31f29678a631074e7c5cff76dd
TylerJahoor <tyler.jahoor@student.tdsb.on.ca>
2025-06-09 15:44:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
 // the link to your model provided by Teachable Machine export panel
-const URL = "https://teachablemachine.withgoogle.com/models/RCAV-K9Y7/";
+const URL = "https://teachablemachine.withgoogle.com/models/cKMSgOQHx/";
 let model, webcam, ctx, labelContainer, maxPredictions;
 
 let stamps;
